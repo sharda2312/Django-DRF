a03175d1965fcda827656b9e6a6aa1394364ca77
sharda maurya <shardamaurya42570@gmail.com>
2025-04-09 11:46:05 +0530

    crud completed

--- CRUD/views.py
+++ CRUD/views.py
@@ -7,7 +7,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 import io
 
-@api_view(["GET","POST"])
+@api_view(["GET","POST","PUT", "DELETE"])
 def student(request):
     if request.method == 'GET':
         id = request.GET.get("id")
@@ -28,3 +28,47 @@ def student(request):
             print("data saved")
             return Response({"hi":"hello"})
         return Response({"hi":"hello"})
+    
+    if request.method == "PUT":
+        stu = request.data
+        id = stu['id']
+        stu_data = Student.objects.get(id = id)
+        data = studentserialiser(instance=stu_data,data=stu, partial =True)
+        if data.is_valid():
+            data.save()
+            print("data saved")
+            return Response({"hi":"hello"})
+        return Response({"hi":"hello"})
+    
+    
+    if request.method == "DELETE":
+        id = request.data["id"]
+        stu = Student.objects.get(id = id)
+        stu.delete()
+        print("data deleted")
+        return Response({"hi":"hello"})
+    
+    
+    
+"""
+if method.request == "GET":
+    id = request.GET.get("id")
+    stu = Student.objects.get(id = id)
+    serial = studentseriliser(data=stu, safe=false)
+    return response(serial)
+    
+if method.request = "POST":
+    stu = request.data
+    serial = studentserialiser(data=stu)
+    if serial.is_valid():
+        serial.save()
+        
+if method.request = "put":
+    data = request.data
+    id = stu["data"]
+    stu = student.objects.get(id = id)
+    serial = studentserialiser(instance = stu, data = data, partial=True)
+    if serial.is_valid():
+        serial.save()
+
+"""
